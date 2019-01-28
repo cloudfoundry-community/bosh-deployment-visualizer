0ea19e1d0783243f01a0d10070ab9b6c7f6e21b6
Ruben Koster <rkoster@starkandwayne.com>
2019-01-28 16:57:04 +0100

    Added header + improve UX

--- visualize.sh
+++ visualize.sh
@@ -4,9 +4,8 @@
 
 # https://www.planttext.com/
 
-echo -e "@startuml\n"
-
 manifest=${1}
+uml=""
 
 manifest() {
     spruce json ${manifest}
@@ -16,20 +15,32 @@ id_escape() {
     sed 's/-/_/g'
 }
 
+uml() {
+    uml="${uml}\n${1}"
+}
+
+deployment=$(manifest | jq -r '.name')
+
+uml "@startuml\n"
+
+uml "center header
+= Deployment: $deployment
+endheader\n"
+
 for network in $(manifest | jq -r '.instance_groups | map(.networks[].name) | flatten | unique | .[]'); do
     id="$(echo $network | id_escape)"
-    echo -e "\
-        cloud ${id} [
+    uml "\
+    cloud ${id} [
     <b>network: ${network}
-    ]\n" | sed 's/^[ ]*/  /g'
+    ]\n"
 done
 
 for az in $(manifest | jq -r '.instance_groups | map(.azs) | flatten | unique | .[]'); do
-    echo "frame $az {"
+    uml "frame $az {"
     groups=$(manifest | jq -c --arg az $az '.instance_groups | map(select((.instances != 0) and (.azs | contains([$az])))) | map(@base64) | _nwise(3)')
     last_group_link_name=""
     for group in $(echo "$groups"); do
-        echo "together {"
+        uml "together {"
         for instance in $(echo "$group" | jq -c -r '.[]'); do
             _jq() {
                 echo "${instance}" | base64 --decode | jq -r "$@"
@@ -40,35 +51,34 @@ for az in $(manifest | jq -r '.instance_groups | map(.azs) | flatten | unique | 
             iazs=$(_jq -c '. as $ig | [$ig.azs, ([[range(0;$ig.instances)] | _nwise($ig.instances / ($ig.azs | length) | ceil)] | map(length))] | transpose')
             instances=$(echo $iazs | jq -r --arg az $az 'map(select(.[0] == $az))[0][1] // 0')
 
-            echo -e "\
+            uml "\
             node ${id} [
             <b>${name} ${instances}
             ----
             ${jobs}
-            ]\n" | sed 's/^[ ]*/  /g'
-            # echo "$id -0- default_$az"
+            ]\n"
+
             for network in $(_jq '.networks[].name'); do
                 nid=$(echo $network | id_escape)
-                echo "$id -0- $nid"
+                uml "$id -0- $nid"
             done
         done
         last_group_current_name="$(echo "$group" | jq -r '.[0]' | base64 --decode | jq -r '.name' | id_escape)_$az"
         if [ "${last_group_last_name}" != "" ]; then
-            echo "${last_group_last_name} -[hidden]---> ${last_group_current_name}"
+            uml "${last_group_last_name} -[hidden]---> ${last_group_current_name}"
         fi
         last_group_last_name="${last_group_current_name}"
 
-        echo "}" # close together
+        uml "}" # close together
     done
-    echo "}" # close az frame
+    uml "}" # close az frame
 done
 
-# echo "z1 -[hidden]----> z2"
-# echo "z2 -[hidden]----> z3"
-# echo "default_z1 -0- default_z2"
-# echo "default_z1 -0- default_z3"
-# echo "default_z2 -0- default_z1"
-# echo "default_z2 -0- default_z3"
-# echo "default_z3 -0- default_z1"
-# echo "default_z3 -0- default_z2"
-echo "@enduml"
+uml "@enduml"
+
+if [[ ! -z "$DEBUG" ]]; then
+    echo -e "${uml}"
+fi
+
+echo -e "${uml}" | plantuml -p > "${deployment}.png"
+echo "Generated $(pwd)/${deployment}.png"
